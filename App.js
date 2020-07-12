a0f3f3bdb8ba0be867fdc1785ae0857f049522b0
Ahmed Ghandour <ghandourelagati@gmail.com>
2020-07-12 10:15:59 +0200

    First commit

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+// App.js
 
-import SignupScreen from './screens/SignupScreen';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App: () => React$Node = () => {
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Measurement from './components/Measurement';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View>
-      <SignupScreen />
-    </View>
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{title: 'Signup'}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={({title: 'Login'}, {headerLeft: null})}
+      />
+      <Stack.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={({title: 'Dashboard'}, {headerLeft: null})}
+      />
+      <Stack.Screen
+        name="Measurement"
+        component={Measurement}
+        options={({title: 'Measurement'}, {headerLeft: null})}
+      />
+    </Stack.Navigator>
   );
-};
+}
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
