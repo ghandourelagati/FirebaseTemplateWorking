871a600da09fcb7070da877367d699f25eddd211
Ahmed Ghandour <ghandourelagati@gmail.com>
2020-07-14 20:26:43 +0200

    Measurements medical half way

--- components/Measurement.js
+++ components/Measurement.js
@@ -14,16 +14,105 @@ import firebase from '../database/firebase';
 
 const DATA = [
   {
-    title: 'Main dishes',
-    data: ['Pizza', 'Burger', 'Risotto'],
+    title: 'Adrenal gland',
+    data: ['<1 cm thick', '4-6 cm length'],
   },
   {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+    title: 'Aorta',
+    data: ['<3 cm diameter'],
   },
   {
-    title: 'Drinks',
-    data: ['Water', 'Coke', 'Beer'],
+    title: 'Appendix',
+    data: ['on CT <6 mm caliber'],
+  },
+  {
+    title: 'Atlantodental distance',
+    data: ['adults: <3 mm', 'children: <5 mm'],
+  },
+  {
+    title: 'Azygous vein',
+    data: ['on erect chest x-ray <10 mm diameter'],
+  },
+  {
+    title: 'Bladder wall',
+    data: ['<3 mm (well-distended state)'],
+  },
+  {
+    title: 'Boehler angle',
+    data: ['20-40o'],
+  },
+  {
+    title: 'Capitolunate angle',
+    data: ['<30o'],
+  },
+  {
+    title: 'Carinal angle',
+    data: ['<60-70o'],
+  },
+  {
+    title: 'Colon',
+    data: ['lumen: <5 cm', 'wall: <3 mm'],
+  },
+  {
+    title: 'Common bile duct',
+    data: [
+      '<7 mm and add 1 mm for each decade over age of 60 years',
+      'up to 10 mm postcholecystectomy',
+    ],
+  },
+  {
+    title: 'Diaphragm (right dome is usually higher than left)',
+    data: ['difference between right and left: <3 cm'],
+  },
+  {
+    title: 'Endometrial thickness',
+    data: ['premenopausal: 3-15 mm', 'postmenopausal: <6 mm'],
+  },
+  {
+    title: 'Esophagus wall',
+    data: ['<3 mm (with distended lumen)'],
+  },
+  {
+    title: 'Gallbladder wall',
+    data: ['<3 mm (well-distended)'],
+  },
+  {
+    title: 'Heart (cardiothoracic ratio)',
+    data: ['<0.55'],
+  },
+  {
+    title: 'Inferior vena cava',
+    data: ['<28 mm'],
+  },
+  {
+    title: 'Internal carotid artery',
+    data: [
+      'peak systolic velocity (PSV): <125 cm/s',
+      'end-diastolic velocity (EDV): <40 cm/s',
+    ],
+  },
+  {
+    title: 'Kidneys',
+    data: ['8-10 cm x 4-6 cm'],
+  },
+  {
+    title: 'Liver span',
+    data: ['<15 cm'],
+  },
+  {
+    title: 'Lymph nodes',
+    data: [
+      'mediastinal: <10 mm in short axis',
+      'retrocrural: <6 mm in short axis',
+    ],
+  },
+  {
+    title: 'Ovarian follicle',
+    data: ['<2.5-3 cm'],
+  },
+  {
+    title: 'Ovaries',
+    data: ['volume premenopausal: <18 mL', 'postmenopausal: <8 mL'],
   },
   {
     title: 'Desserts',
